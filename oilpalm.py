ce553681d480d0b0c4d81af4df5846b9d7f2ba84
Nabilah Selayanti <168091013+Selayanti@users.noreply.github.com>
2025-05-27 10:48:55 +0700

    Update oilpalm.py

--- oilpalm.py
+++ oilpalm.py
@@ -94,7 +94,7 @@ with st.sidebar:
             image = Image.open(camera_photo)
 
     # Created by section
-    profile_img = Image.open("foto.jpg")
+    profile_img = Image.open("foto1.jpg")
     st.markdown(
         f"""
         <style>
